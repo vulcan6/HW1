17e033438ed03ae3b1ada70dfb3858b599f2beea
vulcan6 <70494084+vulcan6@users.noreply.github.com>
2020-09-15 23:02:48 -0500

    Add files via upload

---
+++ Zylab 1.20.py
@@ -0,0 +1,14 @@
+#Name: Erick Jimenez
+#PSID: 1463639
+#Assignment: Zylab 1.20
+
+user_num = input('Enter integer:\n')
+user_num = int(user_num)
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num*user_num)
+print('And', user_num, 'cubed is', user_num*user_num*user_num, "!!")
+
+user_num2 = input('Enter another integer:\n')
+user_num2 = int(user_num2)
+print(user_num, '+', user_num2, 'is', user_num+user_num2)
+print(user_num, '*', user_num2, 'is', user_num*user_num2)
---
+++ Zylab 1.21.1.py
@@ -0,0 +1,9 @@
+#The assignment is to get an integer from input, and output that integer squared, ending with newline. Below is a program that's been nearly completed for you.
+
+# Name: Erick Jimenez
+# PSID 1463639
+
+userNum = int(input())
+userNumSquared = userNum * userNum
+
+print(userNumSquared)
